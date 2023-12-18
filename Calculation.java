987821abcebd752a556411418d92e74c1a5252d6
Felix <felixpraveen1076@gmail.com>
2023-12-18 13:06:40 +0530

    Calculation class

--- Calculation.java
+++ Calculation.java
@@ -2,11 +2,15 @@ public class Calculation {
     static Integer a=10;
 
     static Integer b=10;
+    static Integer d=90;
 
     public static void main(String args[]){
 
         Integer c=a+b;
         System.out.println(c);
+
+        System.out.println(d);
+
 
     }
 }
