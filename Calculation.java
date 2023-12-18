8c385c60a5c69487398aa07698d27a38ce968713
Delphina G <delphina@ideassion.com>
2023-12-18 12:48:27 +0530

    Calculation class.

---
+++ Calculation.java
@@ -0,0 +1,12 @@
+public class Calculation {
+    static Integer a=10;
+
+    static Integer b=10;
+
+    public static void main(String args[]){
+
+        Integer c=a+b;
+        System.out.println(c);
+
+    }
+}
